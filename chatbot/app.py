9d568ac7b4b65d3f7a32d9e53a588acb00ac9cd9
adejumoridwan <adejumo999@gmail.com>
2023-07-03 09:10:46 +0100

    deploy app with shinyapp.io

--- chatbot/app.py
+++ chatbot/app.py
@@ -2,9 +2,10 @@ from shiny import App, render, ui, reactive
 import shinyswatch
 import os
 import openai
+from dotenv import load_dotenv
 import asyncio
 import time
-from dotenv import load_dotenv
+
 
 load_dotenv(".env")
 
@@ -25,7 +26,6 @@ def chatbot(input):
     messages.append({"role": "assistant", "content": reply})
     return reply
 
-
 app_ui = ui.page_fluid(
    ui.page_navbar(
         shinyswatch.theme.vapor(),
@@ -37,22 +37,20 @@ app_ui = ui.page_fluid(
    )
 )
 
-
 def server(input, output, session):
     @output
     @render.text
     @reactive.event(input.submit)
     async def text_output():
         with ui.Progress(min=1, max=20) as p:
-            p.set(message="Getting answer", detail="This may take a while...")
+            p.set(detail="This may take a while...")
 
             for i in range(1, 50):
                 p.set(i, message="Getting answer")
                 await asyncio.sleep(1)
 
 
-        output = chatbot(input.text_input())
-        return output
-
+            output = chatbot(input.text_input())
+            return output
 
 app = App(app_ui, server)
